30baf09566b27782de86a5f6d6ccd390664d75f1
Jair <jairdvargas@icloud.com>
2022-02-28 17:31:12 -0400

    FE: Se instalo y limpio React y bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Variables de proceso</h1>
+    </div>
+  );
+}
+
+export default App;
